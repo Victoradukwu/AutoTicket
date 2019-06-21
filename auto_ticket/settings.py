056d9ff7dc1e28e21a6f5e450772bade979c9881
Victoradukwu <vicads01@gmail.com>
2019-06-21 12:06:02 +0100

    Update settings.py

--- auto_ticket/settings.py
+++ auto_ticket/settings.py
@@ -107,9 +107,9 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST', 'localhost'),
-        'PORT': os.environ.get('DB_PORT', '5432')
-    }
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432')
+    },
 }
 
 
